2402cbebb1ebb7db50d9c383b5bb5f1a30ffe5c4
yanteon <var.yas.ar.w11@gmail.com>
2023-07-29 10:13:21 -0400

    Se añade un array de String para que la maquina eliga una opcion aleatoriamente

--- src/main/kotlin/RockPaperScissors.kt
+++ src/main/kotlin/RockPaperScissors.kt
@@ -2,4 +2,7 @@ fun main() {
     //variables y entrada
     println("Ingrese el una opcion (piedra, papel o tijeras)")
     val opcionDeUsuario = readln()
+
+    //se añadio un array de opciones para que la maquina eliga aleatoriamente una opcion
+    val tipoDeOpciones: Array<String> = arrayOf("Rock", "Papers", "Scissors")
 }
